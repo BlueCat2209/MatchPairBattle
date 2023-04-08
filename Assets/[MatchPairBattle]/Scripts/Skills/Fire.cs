7bd8de1a6daab63bfaa33543c5223518a646a9c2
BlueCat2209 <0398812059a@gmail.com>
2023-04-08 20:31:00 +0700

    Create complete fire skill and transfer data between players when casting skill

--- Assets/[MatchPairBattle]/Scripts/Skills/Fire.cs
+++ Assets/[MatchPairBattle]/Scripts/Skills/Fire.cs
@@ -9,24 +9,61 @@ namespace ElementSkill
     {
         [Header("SKILL PROPERTIES")]
         [SerializeField] GameObject m_firePrefab;
+        [SerializeField] float m_speed;
+
+        private int m_fireAmount;
         
+        private List<Vector3> m_fireBallTarget = new List<Vector3>();        
+        private List<Transform> m_fireBallTransform = new List<Transform>();        
+        private List<Pikachu.AnimalButton> m_animalButtons = new List<Pikachu.AnimalButton>();
 
         void Update()
         {
-            
+            if (!m_isCasting) return;
+
+            int countDown = 0;
+            for (int i = 0; i < m_fireAmount; i++)
+            {
+                if (Vector3.Distance(m_fireBallTransform[i].position, m_fireBallTarget[i]) > 0.1f)
+                {
+                    m_fireBallTransform[i].position = Vector3.MoveTowards(m_fireBallTransform[i].position, m_fireBallTarget[i], m_speed * Time.deltaTime);
+                }
+                else
+                {
+                    countDown++;
+                    m_fireBallTransform[i].position = m_fireBallTarget[i];
+                }
+            }            
+            if (countDown == m_fireAmount)
+            {
+                m_isCasting = false;
+                for (int i = 0; i < m_fireAmount; i++)
+                {
+                    m_targetTable.HideButton(m_animalButtons[i]);
+                    Destroy(m_fireBallTransform[i].gameObject, 2f);
+                }
+                Destroy(gameObject, 2f);
+            }
         }
 
-        public virtual void StartSkill(List<Pikachu.AnimalButton> skillTargets)
+        public virtual void StartSkill(List<byte[]> skillTargets, Vector3 skillStart, bool isPlayerCast = true)
         {
-            for (int i = 0; i < 6; i++)
+            m_isCasting = true;                        
+
+            for (int i = 0; i < skillTargets.Count; i++)
             {
-                if (i < skillTargets.Count - 1)
-                {
-                    var target = skillTargets[i].transform.position;
-                    var skill = Instantiate(m_firePrefab, transform);
-                    skill.transform.position = target;
-                    Destroy(skill, 2f);
-                }
+                m_targetTable = (isPlayerCast) ? GameManagement.Instance.PlayerTable : GameManagement.Instance.OpponentTable;
+                var animalButton = m_targetTable.Table[skillTargets[i][0], skillTargets[i][1]];
+                m_animalButtons.Add(animalButton);
+                m_fireBallTarget.Add(animalButton.transform.position);
+            }
+            m_fireAmount = (skillTargets.Count >= 6) ? 6 : (skillTargets.Count / 2) * 2;
+            for (int i = 0; i < m_fireAmount; i++)
+            {               
+                var skill = Instantiate(m_firePrefab, transform);
+                    
+                skill.transform.position = skillStart;
+                m_fireBallTransform.Add(skill.transform);
             }
         }
     }
